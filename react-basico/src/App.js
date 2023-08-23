ca6ac2c9405ea660b6f7fb365d7bbc97d38fd458
Fernando Randi <feferandii@gmail.com>
2023-08-23 10:19:19 -0300

    Primeiro contato com JS

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Component01 from "./components/Component01";
+import Component02 from "./components/Component02";
 
 function App() {
   return (
     <div className="App">
       <h1>React Mano</h1>
       <Component01 />
+      <Component02 />
     </div>
   );
 }
---
+++ react-basico/src/components/Component02.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Component02 = () => {
+    const variavel = 2 + 2
+  return (
+    <div>
+        Component02
+        <p>{variavel}</p>
+        <p>Crsitian</p>
+    </div>
+  )
+}
+
+export default Component02
