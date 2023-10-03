ae0b2de0881fbbcbd9ca322dc7b449afc9bf7645
Fernando Randi <feferandii@gmail.com>
2023-10-03 18:49:53 -0300

    Criada a interface da calculadora

---
+++ ecomerce/src/components/Operadores.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Operadores = ({ operador, simbolo }) => {
+  // Estilos CSS inline com base no símbolo
+  const buttonStyles = {
+    fontSize: '58px',
+    borderRadius: '5px',
+    boxShadow: '0px 2px 5px rgba(0, 0, 0, 3)', // Sombra na parte inferior
+    backgroundColor: simbolo === '+' ? '#00b1FF' : simbolo === '-' ? '#FFFF00' : simbolo === 'X' ? '#4CAF50' : '#EB0000',
+    color: simbolo === '-' ? 'black' : 'white',
+    border: 'none',
+    width: '91px',
+    height: '106px',
+    cursor: 'pointer',
+  };
+
+  return (
+    <div>
+      <button style={buttonStyles}>{simbolo}</button>
+    </div>
+  );
+};
+
+export default Operadores;
--- ecomerce/src/pages/Calculator.js
+++ ecomerce/src/pages/Calculator.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Operadores from "../components/Operadores";
+import "./Calculator.css";
 
 const Calculator = () => {
   return (
-    <div>Calculator</div>
-  )
-}
+    <fieldset>
+      <legend>Minha Calculadora</legend>
+      <div class="calculadora">
+        <div class="linha">
+          <div class="coluna">
+            <input className="entrada" type="text" />
+          </div>
+          <div class="coluna">
+            <input className="entrada" type="text" />
+          </div>
+          <div class="coluna operadores">
+            <div class="coluna operador">
+              <Operadores simbolo="+" />
+            </div>
+            <div class="coluna operador">
+              <Operadores simbolo="-" />
+            </div>
+            <div class="coluna operador">
+              <Operadores simbolo="X" />
+            </div>
+            <div class="coluna operador">
+              <Operadores simbolo="÷" />
+            </div>
+          </div>
+          <div class="coluna igual">=</div>
+          <div class="coluna">
+            <input className="resultado" type="text" />
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  );
+};
 
-export default Calculator
+export default Calculator;
